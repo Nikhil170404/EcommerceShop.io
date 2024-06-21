a6725717c3e610dabe114dd263eb785cab8aecf8
Prashant Singh <94859943+Nikhil170404@users.noreply.github.com>
2024-06-22 02:20:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
 import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -10,33 +10,25 @@ import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-dotenv.config();
-
-const app = express();
 const port = process.env.PORT || 5000;
 
-// Connect to MongoDB
 connectDB();
 
-// CORS middleware
-app.use(cors({
-  origin: 'https://frontend-or4y5rjna-nikhil170404s-projects.vercel.app',
-  methods: ['GET', 'POST'], // Allow only GET and POST requests, adjust as necessary
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow additional headers
-}));
+const app = express();
 
-// Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// API routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
-// Serve static files in production
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
   app.use('/uploads', express.static('/var/data/uploads'));
@@ -46,7 +38,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
-  // Development mode
   const __dirname = path.resolve();
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get('/', (req, res) => {
@@ -54,11 +45,9 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
-});
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
