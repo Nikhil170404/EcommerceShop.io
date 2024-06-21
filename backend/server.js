773051fb6da8ba82c36d2b52f92497b3a0d3e7e3
Prashant Singh <94859943+Nikhil170404@users.noreply.github.com>
2024-06-22 02:10:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-dotenv.config();
+import cors from 'cors'; // Import cors package
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -10,11 +10,16 @@ import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
+dotenv.config();
+
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
